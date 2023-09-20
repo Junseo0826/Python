57054c43f1fdddfcf80b1c2483ef0135ca0110ec
이준서 <127255937+Junseo0826@users.noreply.github.com>
2023-09-20 11:56:07 +0900

    2023년 09월 20일 실습 내용

---
+++ 4장/07_rand2_이준서.py
@@ -0,0 +1,56 @@
+'''
+    작성일 : 2023년 09월 20일
+    작성자 : 컴퓨터공학부 202395025 이준서
+    설명 : 선택문 if-else
+            random을 이용한 가위바위보 게임.
+            
+            random함수로 생성된 정수를 가지고 게임을 진행합니다.
+            0 ==> 가위
+            1 ==> 바위
+            2 ==> 보
+            
+            두 명의 플레이어의 이름을 입력 받아 가위바위보 게임을 진행합니다.
+'''
+import random       # random함수 가져오기.
+
+print(":: 가위 바위 보 게임 시작 ::")
+
+player1 = input("Player1의 이름 : ")
+player2 = input("Player2의 이름 : ")
+
+num1 = random.randrange(3) # player1의 랜덤 수
+num2 = random.randrange(3) # player2의 랜덤 수
+
+# player1의 가위 바위 보 출력
+print(player1," : ", end='')
+if num1 == 0:
+    print("가위")
+elif num1 == 1:
+    print("바위")
+else :
+    print("보")    
+
+# player2의 가위 바위 보 출력
+print(player2," : ", end='')
+if num2 == 0:
+    print("가위")
+elif num2 == 1:
+    print("바위")
+else :
+    print("보")
+    
+if num1 == num2 :
+    print("비겼습니다.")
+elif num1 == 0 and num2 == 1:
+    print(f"{player2}가 이겼습니다.")
+elif num1 == 1 and num2 == 2:
+    print(f"{player2}가 이겼습니다.")
+elif num1 == 2 and num2 == 0:
+    print(f"{player2}가 이겼습니다.")
+elif num1 == 0 and num2 == 2:
+    print(f"{player1}가 이겼습니다.")
+elif num1 == 1 and num2 == 0:
+    print(f"{player1}가 이겼습니다.")
+elif num1 == 2 and num2 == 1:
+    print(f"{player1}가 이겼습니다.")
+
